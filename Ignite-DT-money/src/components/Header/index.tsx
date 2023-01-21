a1920b874cfdac0c4b8bace0cafa5bc1f3f6d96d
Bruno <bemds1@aluno.ifnmg.edu.br>
2023-01-21 19:04:39 -0300

    modal complete

--- Ignite-DT-money/src/components/Header/index.tsx
+++ Ignite-DT-money/src/components/Header/index.tsx
@@ -1,13 +1,22 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
-import logoImg from '../../assets/Logo.svg'
-export function Header(){
-    return (
-        <HeaderContainer>
-                <HeaderContent>
-                    <img src={logoImg} alt="" />
-                    <NewTransactionButton>Nova Transação</NewTransactionButton>
-                </HeaderContent>
-        </HeaderContainer>
-    )
+import * as Dialog from "@radix-ui/react-dialog";
+import logoImg from "../../assets/Logo.svg";
+import { NewTransactionModal } from "../NewTransactionModal";
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <HeaderContent>
+        <img src={logoImg} alt="" />
+
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova Transação</NewTransactionButton>
+          </Dialog.Trigger>
+
+        <NewTransactionModal/>
+        </Dialog.Root>
+      </HeaderContent>
+    </HeaderContainer>
+  );
 }
-    
